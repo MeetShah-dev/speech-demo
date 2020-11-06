0ee2f3b63ac351e254b470b8fe48f28fdcb6ff48
meet <mshah@argusoft.in>
2020-11-06 12:09:36 +0530

    speech-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx'
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -9,6 +11,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,8 +20,10 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
+    TextToSpeech,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
+
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private speechRecognition: SpeechRecognition, private changeDetectorRef: ChangeDetectorRef, private textToSpeech: TextToSpeech) { }
+  speechData: string = 'Default';
+  ngOnInit() {
 
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission()
+            .then(
+              () => console.log('Granted'),
+              () => console.log('Denied')
+            )
+        }
+
+      });
+
+  }
+  startListening() {
+    this.speechRecognition.startListening()
+      .subscribe(
+        async (matches: Array<string>) => {
+          const matchData = matches[0];
+          if (matchData.toLowerCase() === 'what is my name') {
+            this.speechData = 'Harsh Patel';
+          } else if (matchData.toLowerCase() === 'what is my profit') {
+            this.speechData = '2.5 $';
+          } else {
+            this.speechData = 'Default Message'
+          }
+          await this.textToSpeech.speak(this.speechData);
+          this.changeDetectorRef.detectChanges();
+        },
+        (onerror) => console.log('error:', onerror)
+      )
+  }
 }
